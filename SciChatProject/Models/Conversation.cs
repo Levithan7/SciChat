7d1af0938a1d3c0169a31b28522cef432c680d2c
Till6becker <155962015+Till6becker@users.noreply.github.com>
2024-02-07 13:13:53 +0100

    correction

--- SciChatProject/Models/Conversation.cs
+++ SciChatProject/Models/Conversation.cs
@@ -8,24 +8,13 @@
 
         public List<Message> GetMessages()
         {
-            return DataBaseHelper.GetObjects<Message>().Where(x => x.ConversationID==id).Select(x => new Message { Content = x.Content }).ToList();
+            return DataBaseHelper.GetObjects<Message>().Where(x => x.ConversationID==id).ToList();
         }
 
         public List<User> GetUsers()
         {
             return DataBaseHelper.GetObjects<UserConversationLink>().Where(x=>x.ConversationID==id).Select(x=>new User { id=x.UserID }).ToList();
         }
-
-
-        public List<User> GetUser(string m)
-		{
-			return DataBaseHelper.GetObjects<Message>().Where(x => x.ConversationID == id).Where(x => x.Content == m).Select(x => new User { id = x.UserID }).ToList();
-		}
-
-		public List<User> GetUsersName(int uid)
-		{
-			return DataBaseHelper.GetObjects<User>().Where(x => x.id == uid).Select(x => new User { UsernameTEST = x.UsernameTEST }).ToList();
-		}
 
 		public void AddUserToConversation(User user)
         {
