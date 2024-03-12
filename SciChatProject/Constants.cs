18631768251092f2e680b32a4b777b64b51ebd0f
Till6becker <tlbeck666@outlook.de>
2024-03-12 21:36:15 +0100

    final version from me

--- SciChatProject/Constants.cs
+++ SciChatProject/Constants.cs
@@ -2,6 +2,6 @@
 {
     public class Constants
     {
-        public static string CONNECTIONSTRING = "Data Source=HBG-T440-2\\SQLEXPRESS;Initial Catalog=scichat;Connect Timeout=30;Encrypt=False;Trusted_Connection=true";
+        public static string CONNECTIONSTRING = "Data Source=DESKTOP-7LA7N70\\SQLEXPRESS;Initial Catalog=scichat;Connect Timeout=30;Encrypt=False;Trusted_Connection=true";
     }
 }
--- SciChatProject/Models/Message.cs
+++ SciChatProject/Models/Message.cs
@@ -16,6 +16,8 @@ namespace SciChatProject.Models
 
         public int id { get; set; }
 
+        public DateTime eventtime { get; set; }
+
         [SQLProperty(Name="content")] public string Content { get; set; }
         [SQLProperty(Name="userid")] public int UserID { get; set; }
         [SQLProperty(Name="conversationid")] public int ConversationID { get; set; }
