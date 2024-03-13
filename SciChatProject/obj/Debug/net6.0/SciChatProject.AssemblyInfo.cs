a9a3bc9e6d32b5d997ddbced287dcb8ff56988d1
Levithan7 <levijo.ems@gmail.com>
2024-03-13 01:18:47 +0100

    Merging fixed and slight update on security

--- SciChatProject/obj/Debug/net6.0/SciChatProject.AssemblyInfo.cs
+++ SciChatProject/obj/Debug/net6.0/SciChatProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SciChatProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3884839ad4b08f9c46e1048d0a1b0e41c1a657")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d5f6a7cba89999d6ae7b3f8e7de87fddc32951")]
 [assembly: System.Reflection.AssemblyProductAttribute("SciChatProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SciChatProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/Controllers/ServerController.cs
+++ Server/Controllers/ServerController.cs
@@ -5,6 +5,7 @@ using SciChatProject;
 using SciChatProject.Models;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.VisualBasic;
 
 namespace Server.Controllers
 {
@@ -20,13 +21,20 @@ namespace Server.Controllers
             if(!SciChatProject.Models.User.PasswordTrue(u, p))
                 return Unauthorized($"Wrong credentials for user {u}");
 
-            var json = JsonSerializer.Serialize(Conversation.GetConversationByID(id));
+			var conversation = Conversation.GetConversationByID(id);
+			if (!conversation.GetUsers().Select(x => x.id).Contains(u))
+				return Unauthorized($"The bot is not part of the Conversation {id} and thus not allowed to access it");
+
+			var json = JsonSerializer.Serialize(Conversation.GetConversationByID(id));
             return Ok(json);
         }
 
         [HttpGet("convsByUserID")]
         public IActionResult GetConversationsByUserID(int userid, int u, string p)
         {
+            if (userid != u)
+                return Unauthorized($"The bot (ID: {u}) trying to access the conversation of the user (ID: {userid}) does not match its ID");
+
 			if (!SciChatProject.Models.User.PasswordTrue(u, p))
 				return Unauthorized($"Wrong credentials for user {u}");
 
@@ -40,6 +48,10 @@ namespace Server.Controllers
 			if (!SciChatProject.Models.User.PasswordTrue(u, p))
 				return Unauthorized($"Wrong credentials for user {u}");
 
+			var conversation = Conversation.GetConversationByID(id);
+			if (!conversation.GetUsers().Select(x => x.id).Contains(u))
+				return Unauthorized($"The bot is not part of the Conversation {id} and thus not allowed to access it");
+
 			var json = JsonSerializer.Serialize(Conversation.GetConversationByID(id).GetUsers());
             return Ok(json);
         }
@@ -50,6 +62,10 @@ namespace Server.Controllers
 			if (!SciChatProject.Models.User.PasswordTrue(u, p))
 				return Unauthorized($"Wrong credentials for user {u}");
 
+			var conversation = Conversation.GetConversationByID(id);
+			if (!conversation.GetUsers().Select(x => x.id).Contains(u))
+				return Unauthorized($"The bot is not part of the Conversation {id} and thus not allowed to access it");
+
 			var json = JsonSerializer.Serialize(Conversation.GetConversationByID(id).GetMessages());
             return Ok(json);
         }
@@ -57,12 +73,16 @@ namespace Server.Controllers
         [HttpGet("userReceivedMessagesByID")]
         public IActionResult GetUserReceivedMessages(int userid, int u, string p, bool includeOwn=false)
         {
+			if (userid != u)
+				return Unauthorized($"The bot (ID: {u}) trying to access the conversation of the user (ID: {userid}) does not match its ID");
+
 			if (!SciChatProject.Models.User.PasswordTrue(u, p))
 				return Unauthorized($"Wrong credentials for user {u}");
 
 			var messages = SciChatProject.Models.User.GetUserByID(userid).GetConversations().SelectMany(x=>x.GetMessages()).ToList();
             if (!includeOwn)
                 messages.RemoveAll(x=>x.UserID==userid);
+
             var json = JsonSerializer.Serialize(messages);
             return Ok(json);
         }
@@ -70,6 +90,9 @@ namespace Server.Controllers
         [HttpGet("userSentMessagesByID")]
         public IActionResult GetUserSentMessages(int userid, int u, string p)
         {
+			if (userid != u)
+				return Unauthorized($"The bot (ID: {u}) trying to access the conversation of the user (ID: {userid}) does not match its ID");
+
 			if (!SciChatProject.Models.User.PasswordTrue(u, p))
 				return Unauthorized($"Wrong credentials for user {u}");
 
@@ -81,8 +104,15 @@ namespace Server.Controllers
         [HttpPost("addmessage")]
         public IActionResult AddMessage(int userid, int conversationid, string content, int u, string p)
         {
+			if (userid != u)
+				return Unauthorized($"The bot (ID: {u}) trying to access the conversation of the user (ID: {userid}) does not match its ID");
+
 			if (!SciChatProject.Models.User.PasswordTrue(u, p))
 				return Unauthorized($"Wrong credentials for user {u}");
+
+			var conversation = Conversation.GetConversationByID(conversationid);
+			if (!conversation.GetUsers().Select(x => x.id).Contains(u))
+				return Unauthorized($"The bot is not part of the Conversation {conversationid} and thus not allowed to access it");
 
 			Message.SendMessage(content, userid, conversationid);
             return Ok("Message Sent!");
